6f018c03bc82ea8921950fd0c6328e17f238b10b
Liuda Arkavenko <arkavenko18@gmail.com>
2021-04-10 09:53:09 +0300

    fixed hover for continue btn

--- src/App/Main/Sidebar/Sidebar.js
+++ src/App/Main/Sidebar/Sidebar.js
@@ -30,7 +30,7 @@ const Sidebar =()=>
                             <hr className="title-underline-2"></hr>
                         </div>
                         <div className="post-item">
-                            <div className="post-img try">
+                            <div className="post-img-sidebar">
                                 <img src={image} alt=""/>
                             </div>
                             <div className="post-title">
@@ -89,7 +89,7 @@ const Sidebar =()=>
                             <hr className="title-underline-1"></hr>
                             <hr className="title-underline-2"></hr>
                         </div>
-                        <Carousel  useKeyboardArrows showThumbs={false}>
+                        <Carousel  useKeyboardArrows showThumbs={false}  autoPlay={true} infiniteLoop={true}>
 
                             <div className="post-slide">
                                 <div className="post-img-slide"><img src={image1} alt=""/></div>
